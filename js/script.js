52225bfc9cdf8f16e6009f2bbe40e60996186503
Ezequiel Amurua <ezequielamurua@hotmail.com>
2024-07-29 23:37:39 -0300

    primeros pasos en js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let inversionInicial = 100;
+
+let capitalInvertido = prompt ('¿Cuanto capital quieres Invertir?')
+
+alert ('Genial, entonces estas dispuesto a Invertir ' + capitalInvertido + ' dólares');
+
+let personalizado = 1000;
+
+let alertas = 250;
+
+let totalmensual = personalizado + alertas;
+
+let promoAmigos = personalizado * 2;
+
+let amigos = 2;
+
+let amigosx4 = 4;
+
+let totalmes = personalizado * amigos;
+
+let dividirCostos = totalmes / amigosx4;
+
+let descuento = 150;
+
+let precioConDescuento = totalmes - descuento;
+
+
+let numero = 27;
+
+alert (numero)
